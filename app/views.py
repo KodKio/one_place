c123a9c5d10db85566304bc2b142a7e496abed64
KodKio <stepanovnick.03@gmail.com>
2020-08-01 19:45:13 +0300

    add card decks views at page

--- app/views.py
+++ app/views.py
@@ -6,7 +6,20 @@ import app.parsers as parse
 def home_page(request):
     context = dict()
     dtf = parse.DTFParser()
-    context['dft_news'] = dtf.get_all()
-    context['title'] = "Home Page 1"
+    dtf_news = dtf.get_all()
+    context['dft_news'] = []
+    j = 0
+    row = []
+    for i in range(len(dtf_news)):
+        if j < 3:
+            row.append(dtf_news[i])
+            j += 1
+        else:
+            context['dft_news'].append(row)
+            j = 0
+            row = []
+    if len(row):
+        context['dft_news'].append(row)
+    context['title'] = "DTF"
     return render(request, 'index.html', context)
 
