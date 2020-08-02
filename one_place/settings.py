096dcb0c046f0b1e8165f5fa79d5d52de4ac7f00
KodKio <stepanovnick.03@gmail.com>
2020-08-02 15:49:32 +0300

    now card are links

--- one_place/settings.py
+++ one_place/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static')
+]
 
 INTERNAL_IPS = [
     '127.0.0.1',
