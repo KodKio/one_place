5381f304bb98eb66fed4b47e91149572370105f9
KodKio <stepanovnick.03@gmail.com>
2020-08-03 12:03:10 +0300

    Add static files

--- one_place/urls.py
+++ one_place/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
